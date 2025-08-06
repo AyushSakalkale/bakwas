90b81bf9dfabe80675383f232dd4c3336088627b
AyushSakalkale <ayushsakalkale30@gmail.com>
2025-08-06 12:52:14 +0530

    added cors

--- sendMessage.js
+++ sendMessage.js
@@ -2,14 +2,27 @@
 const Ably = require('ably');
 
 exports.handler = async (event) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+    };
+
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers,
+            body: ''
+        };
+    }
     if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
+        return { statusCode: 405, headers, body: 'Method Not Allowed' };
     }
 
     const { message } = JSON.parse(event.body);
 
     if (!message) {
-        return { statusCode: 400, body: 'Bad Request: Message is required' };
+        return { statusCode: 400, headers, body: 'Bad Request: Message is required' };
     }
 
     const ably = new Ably.Realtime('f-9f7g.HfTCGg:Up57zld2PF6PlAAkdPPwDgyvmoxJHX8FEwZFwAf9910');
@@ -17,9 +30,9 @@ exports.handler = async (event) => {
 
     try {
         await channel.publish('new-message', { text: message });
-        return { statusCode: 200, body: 'Message sent' };
+        return { statusCode: 200, headers, body: 'Message sent' };
     } catch (error) {
-        return { statusCode: 500, body: 'Error sending message' };
+        return { statusCode: 500, headers, body: 'Error sending message' };
     } finally {
         ably.close();
     }
